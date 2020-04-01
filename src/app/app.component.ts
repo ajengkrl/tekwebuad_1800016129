6aea6d22a07d72f22c3ce69807bbc025dde968f7
ajengkrl <ajeng1800016129@webmail.uad.ac.id>
2020-04-02 03:21:01 +0700

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './halaman-utama.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './praktikum129.html',
+  styleUrls: ['./app.component.css'],
+  
 })
 export class AppComponent {
   title = 'tekweb1800016129';
