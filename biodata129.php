adc629a170d3227b15ff358b88ad760899109275
ajengkrl <62319893+ajengkrl@users.noreply.github.com>
2020-06-14 01:32:25 +0700

    Add files via upload

---
+++ biodata129.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+<title>BIODATA1800016129</title>
+</head>
+
+<body>
+<BR>
+
+<table border="1" align="center">
+<tr>
+     <td width="117">NIM</td>
+     <td width="14">:</td>
+     <td width="218"> <?php echo $_POST["NIM"] ?></td>
+</tr>
+<tr>
+     <td>NAMA</td>
+     <td>:</td>
+     <td><?php echo $_POST["NAMA"]?></td>
+</tr>
+<tr>
+     <td>Jenis Kelamin</td>
+     <td>:</td>
+     <td><?php echo $_POST["KELAMIN"]?></td>
+</tr>
+<tr>
+     <td>Program Studi</td>
+     <td>:</td>
+     <td><?php echo $_POST["PRODI"]?></td>
+</tr>
+<tr>
+     <td align="center" colspan="3"><a href="latihan1.html">BACK </a></td>
+</tr>
+</table>
+</body>
+</html>
